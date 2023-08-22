6a0843166f050b154b248cb2f3f47349535d0530
Agony <1197134883@qq.com>
2023-08-22 21:32:58 +0800

    二叉树序列化与反序列化 first commit

---
+++ src/class11/Code02_SerializeAndReconstructTree.java
@@ -0,0 +1,19 @@
+package class11;
+
+/**
+ * @Author Agony
+ * @Create 2023/8/22 21:25
+ * @Version 1.0
+ * // 二叉树的序列化与反序列化
+ */
+public class Code02_SerializeAndReconstructTree {
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
