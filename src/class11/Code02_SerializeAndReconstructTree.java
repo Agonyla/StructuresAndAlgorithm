5d0db6694c6b58c708f692f84d09049836cccc42
Agony <1197134883@qq.com>
2023-08-24 21:26:41 +0800

    按层实现序列化与反序列化

--- src/class11/Code02_SerializeAndReconstructTree.java
+++ src/class11/Code02_SerializeAndReconstructTree.java
@@ -119,7 +119,7 @@ public class Code02_SerializeAndReconstructTree {
         }
         return posDeProcess(stack);
     }
-    
+
     public static Node posDeProcess(Stack<String> stack) {
         String value = stack.pop();
         if (value == null) {
@@ -129,6 +129,90 @@ public class Code02_SerializeAndReconstructTree {
         cur.right = posDeProcess(stack);
         cur.left = posDeProcess(stack);
         return cur;
+    }
+
+
+    // 按层序列化  借助队列实现
+    public static Queue<String> levelSerialize(Node head) {
+        LinkedList<String> queue = new LinkedList<>();
+        levelProcess(head, queue);
+        return queue;
+    }
+
+    public static void levelProcess(Node head, Queue<String> queue) {
+        if (head == null) {
+            queue.add(null);
+        } else {
+            // LinkedList<Node> curQueue = new LinkedList<>();
+            // curQueue.add(head);
+            // while (!curQueue.isEmpty()) {
+            //     Node cur = curQueue.poll();
+            //     if (cur == null) {
+            //         queue.add(null);
+            //         continue;
+            //     } else {
+            //         queue.add(String.valueOf(cur.value));
+            //     }
+            //     curQueue.add(cur.left);
+            //     curQueue.add(cur.right);
+            // }
+            LinkedList<Node> curQueue = new LinkedList<>();
+            curQueue.add(head);
+            queue.add(String.valueOf(head.value));
+            while (!curQueue.isEmpty()) {
+                Node cur = curQueue.poll();
+                if (cur.left != null) {
+                    curQueue.add(cur.left);
+                    queue.add(String.valueOf(cur.left.value));
+                } else {
+                    queue.add(null);
+                }
+                if (cur.right != null) {
+                    curQueue.add(cur.right);
+                    queue.add(String.valueOf(cur.right.value));
+                } else {
+                    queue.add(null);
+                }
+            }
+        }
+    }
+
+
+    // 按层反序列化  借助队列实现
+    public static Node levelDeserialize(Queue<String> queue) {
+        if (queue == null || queue.isEmpty()) {
+            return null;
+        }
+        return levelDeProcess(queue);
+    }
+
+    public static Node levelDeProcess(Queue<String> queue) {
+        Node head = generateNode(queue.poll());
+        LinkedList<Node> curQueue = new LinkedList<>();
+        if (head != null) {
+            curQueue.add(head);
+        }
+        Node cur = null;
+        while (!curQueue.isEmpty()) {
+            cur = curQueue.poll();
+            cur.left = generateNode(queue.poll());
+            cur.right = generateNode(queue.poll());
+            if (cur.left != null) {
+                curQueue.add(cur.left);
+            }
+            if (cur.right != null) {
+                curQueue.add(cur.right);
+            }
+        }
+        return head;
+    }
+
+    // 生成节点
+    public static Node generateNode(String value) {
+        if (value == null) {
+            return null;
+        }
+        return new Node(Integer.parseInt(value));
     }
 
     // 打印队列
@@ -183,11 +267,20 @@ public class Code02_SerializeAndReconstructTree {
         Queue<String> queue1 = inSerialize(n1);
         printQueue(queue1);
 
+
         System.out.println("==========后序===========");
         Queue<String> queue2 = posSerialize(n1);
         printQueue(queue2);
         Node n22 = posDeserialize(queue2);
         printPreBT(n22);
+        System.out.println();
+
+
+        System.out.println("==========按层===========");
+        Queue<String> queue3 = levelSerialize(n1);
+        printQueue(queue3);
+        Node n33 = levelDeserialize(queue3);
+        printPreBT(n33);
         System.out.println();
     }
 
