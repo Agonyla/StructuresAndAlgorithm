645b3746851978987a0bfae03aa07aa09b147de4
Agony <1197134883@qq.com>
2023-08-25 21:25:00 +0800

    Complete Binary Tree first commit

---
+++ src/class12/Code01_IsCBT.java
@@ -0,0 +1,13 @@
+package class12;
+
+/**
+ * @Author Agony
+ * @Create 2023/8/25 20:16
+ * @Version 1.0
+ * 是否是完全二叉树Complete Binary Tree
+ * 按层遍历
+ * 有右无左，则是false
+ * 遇到左右不双全的节点，剩下的节点必须是叶节点
+ */
+public class Code01_IsCBT {
+}
