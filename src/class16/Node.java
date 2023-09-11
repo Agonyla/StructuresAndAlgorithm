1e47a80b679294400fce9b3d64ad389f6bc10444
Agony <1197134883@qq.com>
2023-09-11 21:59:57 +0800

    图的广度优先遍历

--- src/class16/Node.java
+++ src/class16/Node.java
@@ -7,15 +7,13 @@ import java.util.ArrayList;
  * @Create 2023/9/11 20:25
  * @Version 1.0
  * 图的节点信息
- */
-
-/**
  * value:节点值
  * in:有几个节点进入这个节点
  * out:有几个节点出去
  * nexts：出去的节点
  * edges：每条出去的边
  */
+
 public class Node {
     public int value;
     public int in;
