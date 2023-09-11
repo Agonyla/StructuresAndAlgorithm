7a32cecf1fbafddc84864f0245037814d6294fab
Agony <1197134883@qq.com>
2023-09-11 21:58:25 +0800

    图的广度优先遍历

---
+++ src/class16/Code01_BFS.java
@@ -0,0 +1,33 @@
+package class16;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+/**
+ * @Author Agony
+ * @Create 2023/9/11 21:47
+ * @Version 1.0
+ * 图的广度优先遍历
+ */
+public class Code01_BFS {
+
+    public static void bfs(Node start) {
+        if (start == null) {
+            return;
+        }
+        LinkedList<Node> queue = new LinkedList<>();
+        HashSet<Node> set = new HashSet<>();
+        queue.add(start);
+        set.add(start);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            System.out.println(cur.value);
+            for (Node next : start.nexts) {
+                if (!set.contains(next)) {
+                    queue.add(next);
+                    set.add(next);
+                }
+            }
+        }
+    }
+}
