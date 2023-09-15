817e19fabec68613568513831b9b062b965090be
Agony <1197134883@qq.com>
2023-09-15 19:59:25 +0800

    更新

--- src/class16/Code03_TopologicalOrderBFS.java
+++ src/class16/Code03_TopologicalOrderBFS.java
@@ -8,8 +8,6 @@ import java.util.List;
  * @Create 2023/9/14 20:14
  * @Version 1.0
  * <a href="https://www.lintcode.com/problem/127/">拓扑排序</a>
- * 是一种有向无环图，根据先后顺序能够把工作做完
- * 先找到入度为0的节点，把它的影响全部消除，再找到下一个入度为0的节点，消除影响，往复执行
  */
 public class Code03_TopologicalOrderBFS {
 
@@ -23,6 +21,7 @@ public class Code03_TopologicalOrderBFS {
         }
     }
 
+    //TODO: 看看后面视频有没有讲别的方法实现拓扑排序
     public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
 
         return null;
