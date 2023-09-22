438eba77db1954faf1ec09f78300ade5975f4a4d
Agony <1197134883@qq.com>
2023-09-22 10:01:46 +0800

    N皇后

---
+++ src/leetcode/TotalNQueens.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * @Author Agony
+ * @Create 2023/9/22 9:56
+ * @Version 1.0
+ * @describe: 按照国际象棋的规则，皇后可以攻击与之处在同一行或同一列或同一斜线上的棋子。
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n × n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
+ * <p><a href="https://leetcode.cn/problems/n-queens-ii/">N 皇后 II</a></p>
+ */
+public class TotalNQueens {
+    public static int totalNQueens(int n) {
+        return 0;
+    }
+
+
+}
