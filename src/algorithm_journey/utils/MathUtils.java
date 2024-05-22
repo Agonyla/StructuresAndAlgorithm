c9316bae1e6ff383bc05d13a3fa69b190f50e1a9
Agony <gcl1197134883@gmail.com>
2024-05-22 15:58:29 +0800

    新增方法

--- src/algorithm_journey/utils/MathUtils.java
+++ src/algorithm_journey/utils/MathUtils.java
@@ -33,4 +33,50 @@ public class MathUtils {
         Arrays.stream(arr).forEach(num -> System.out.print(num + " "));
         System.out.println();
     }
+
+    /**
+     * 产生一个长度为 n 最大值为 v 的随机数组
+     *
+     * @param n 数组长度
+     * @param v 数组最大值
+     * @return 随机数组
+     */
+    public static int[] randomArray(int n, int v) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = (int) (Math.random() * v) + 1;
+        }
+        return arr;
+    }
+
+    /**
+     * 拷贝数组
+     *
+     * @param arr
+     * @return
+     */
+    public static int[] copyArray(int[] arr) {
+        int length = arr.length;
+        int[] ans = new int[length];
+        System.arraycopy(arr, 0, ans, 0, length);
+        return ans;
+    }
+
+
+    /**
+     * 验证两个数组是否相同
+     *
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public static boolean sameArray(int[] arr1, int[] arr2) {
+        int length = arr1.length;
+        for (int i = 0; i < length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
