afa0f0f91ee4420f1a53a4e88a988e32490d0b15
Agony <gcl1197134883@gmail.com>
2024-05-25 22:19:02 +0800

    用栈实现队列

---
+++ src/algorithm_journey/class014/StackToQueue.java
@@ -0,0 +1,19 @@
+package algorithm_journey.class014;
+
+/**
+ * @author Agony
+ * @create 2024/5/25 22:02
+ * @describe: 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+ * <p>
+ * 实现 MyQueue 类：
+ * <p>
+ * void push(int x) 将元素 x 推到队列的末尾
+ * int pop() 从队列的开头移除并返回元素
+ * int peek() 返回队列开头的元素
+ * boolean empty() 如果队列为空，返回 true ；否则，返回 false
+ * @link: <a href="https://leetcode.cn/problems/implement-queue-using-stacks/description/">用栈实现队列</a>
+ */
+public class StackToQueue {
+
+
+}
