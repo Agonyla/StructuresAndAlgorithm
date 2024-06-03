a2d046462388cb61f612ffbfcff427f44b6e595a
Agony <gcl1197134883@gmail.com>
2024-06-03 21:31:50 +0800

    HashSet and HashMap

---
+++ src/algorithm_journey/class026/HashSetAndHashMap.java
@@ -0,0 +1,21 @@
+package algorithm_journey.class026;
+
+import java.util.HashSet;
+
+/**
+ * @author Agony
+ * @create 2024/6/3 16:20
+ */
+public class HashSetAndHashMap {
+    public static void main(String[] args) {
+
+
+        HashSet<Integer> set = new HashSet<>();
+        set.add(3);
+        set.add(3);
+        set.add(3);
+        set.add(3);
+        System.out.println(set.size());
+
+    }
+}
