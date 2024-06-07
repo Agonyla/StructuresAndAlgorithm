6fdc21d9cdc57dc64232c7682bc224c95ccd815b
Agony <gcl1197134883@gmail.com>
2024-06-07 16:46:58 +0800

    返回大于等于n的最小的2某次方

---
+++ src/algorithm_journey/class031/Code03_Near2power.java
@@ -0,0 +1,17 @@
+package algorithm_journey.class031;
+
+/**
+ * @author Agony
+ * @create 2024/6/7 09:25
+ */
+public class Code03_Near2power {
+
+    public static void main(String[] args) {
+
+
+        int a = Integer.MAX_VALUE;
+        System.out.println("a = " + a);
+        System.out.println(a + 1);
+        // todo
+    }
+}
