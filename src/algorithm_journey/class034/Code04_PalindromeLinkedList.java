5cf4327b0d0071c7448f3e2b61d5bc11cd91abf5
Agony <gcl1197134883@gmail.com>
2024-06-13 11:24:34 +0800

    判断链表是否是回文结构

---
+++ src/algorithm_journey/class034/Code04_PalindromeLinkedList.java
@@ -0,0 +1,18 @@
+package algorithm_journey.class034;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/13 10:36
+ * @describe:
+ * @link:
+ */
+public class Code04_PalindromeLinkedList {
+
+    // todo
+
+    // 判断链表是否是回文结构
+    // 容器方法
+    // 新建一个栈，把链表依次押入栈中
+    // 从头遍历链表，依次弹出栈，判断节点时候一致
+    // 回文则一致，不回文则不一致
+}
