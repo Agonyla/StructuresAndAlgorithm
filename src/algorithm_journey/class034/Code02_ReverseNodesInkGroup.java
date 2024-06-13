6acbdb1139c936fade7db3da6896e561193b7d5e
Agony <gcl1197134883@gmail.com>
2024-06-13 11:24:21 +0800

    每k个节点一组翻转链表

---
+++ src/algorithm_journey/class034/Code02_ReverseNodesInkGroup.java
@@ -0,0 +1,18 @@
+package algorithm_journey.class034;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/13 10:27
+ * @describe:
+ * @link:
+ */
+public class Code02_ReverseNodesInkGroup {
+
+    // todo
+
+    // 每k个节点一组翻转链表
+    // 容器方法
+    // 准备一个数组，把链表按序填充的数组中
+    // 把数组中每 k 个一组做逆序
+    // 再把返回逆序之后数组重新串一下返回头节点
+}
