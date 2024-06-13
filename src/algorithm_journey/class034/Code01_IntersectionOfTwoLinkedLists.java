7feb7e4952615ff7f1b0f4af69a35e307c5877fd
Agony <gcl1197134883@gmail.com>
2024-06-13 11:24:11 +0800

    返回两个无环链表相交的第一个节点

---
+++ src/algorithm_journey/class034/Code01_IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,19 @@
+package algorithm_journey.class034;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/13 10:23
+ * @describe:
+ * @link:
+ */
+public class Code01_IntersectionOfTwoLinkedLists {
+
+
+    // todo
+
+
+    // 返回两个无环链表相交的第一个节点
+    // 容器方法 不需要实现，知道思路就行
+    // 用 hashset 把一条链表的所有节点全部放进去
+    // 然后遍历另一条链表，遍历到当前节点存在 hashset 中就直接返回该节点
+}
