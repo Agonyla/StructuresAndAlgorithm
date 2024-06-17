0ec188c3ac12ad637d32b9f53ef86a59e9004020
Agony <gcl1197134883@gmail.com>
2024-06-17 17:24:38 +0800

    排序链表

--- src/algorithm_journey/class034/Code06_SortList.java
+++ src/algorithm_journey/class034/Code06_SortList.java
@@ -1,10 +1,12 @@
 package algorithm_journey.class034;
 
 /**
+ * 排序链表
+ *
  * @author: Agony
  * @create: 2024/6/13 11:20
- * @describe:
- * @link:
+ * @describe: 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+ * @link: <a href="https://leetcode.cn/problems/sort-list/description/">排序链表</a>
  */
 public class Code06_SortList {
 
@@ -18,9 +20,37 @@ public class Code06_SortList {
     // 因此，递归调用本身也需要 o(log n) 的栈空间。所以，递归版本的归并排序对链表的空间复杂度为 o(log n) 。
     //
     // 递归版本 空间复杂度是  o(1)
-    //不使用递归调用，因此不依赖栈空间。
+    // 不使用递归调用，因此不依赖栈空间。
     // 在处理链表时，非递归归并排序通过迭代方式合并链表的各个部分。
     // 由于链表的节点合并可以通过调整节点间的指针完成，不需要额外的存储空间来创建合并后的链表。
     // 因此，非递归归并排序的空间复杂度为  o(1) ，即常数级空间复杂度。
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+
+
+        return null;
+    }
+
+
+    public static void merge() {
+    }
+
 
 }
