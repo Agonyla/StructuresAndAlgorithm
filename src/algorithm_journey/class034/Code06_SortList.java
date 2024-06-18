d4af348df3f4a217f0aa112c04f72b150dce9502
Agony <gcl1197134883@gmail.com>
2024-06-18 10:34:54 +0800

    排序链表

--- src/algorithm_journey/class034/Code06_SortList.java
+++ src/algorithm_journey/class034/Code06_SortList.java
@@ -1,5 +1,8 @@
 package algorithm_journey.class034;
 
+import algorithm_journey.utils.MathUtils;
+import algorithm_journey.utils.MathUtils.ListNode;
+
 /**
  * 排序链表
  *
@@ -11,7 +14,18 @@ package algorithm_journey.class034;
 public class Code06_SortList {
 
 
-    // todo
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(1);
+        // head.next.next.next = new ListNode(4);
+        // head.next.next.next.next = new ListNode(0);
+        MathUtils.printList(head);
+
+        ListNode listNode = sortList(head);
+        MathUtils.printList(listNode);
+    }
+    
 
     // 排序链表
     // 递归版本，空间复杂度是 o(logn)
@@ -25,32 +39,119 @@ public class Code06_SortList {
     // 由于链表的节点合并可以通过调整节点间的指针完成，不需要额外的存储空间来创建合并后的链表。
     // 因此，非递归归并排序的空间复杂度为  o(1) ，即常数级空间复杂度。
 
-    public static class ListNode {
-        int val;
-        ListNode next;
+    // public static class ListNode {
+    //     int val;
+    //     ListNode next;
+    //
+    //     ListNode() {
+    //     }
+    //
+    //     ListNode(int val) {
+    //         this.val = val;
+    //     }
+    //
+    //     ListNode(int val, ListNode next) {
+    //         this.val = val;
+    //         this.next = next;
+    //     }
+    // }
 
-        ListNode() {
-        }
 
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+    /**
+     * 链表排序
+     *
+     * @param head 链表头节点
+     * @return
+     */
+    public static ListNode sortList(ListNode head) {
+        return mergeSort(head);
     }
 
-    public ListNode sortList(ListNode head) {
 
+    public static ListNode mergeSort(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = findMid(head);
+        // 从中间节点的下一个节点开始
+        ListNode next = mid.next;
+        mid.next = null;
 
-        return null;
+        // 把左边的链表排序
+        ListNode left = mergeSort(head);
+        // 把右边的链表排序
+        ListNode right = mergeSort(next);
+        // 两边排序
+        return merge(left, right);
     }
 
 
-    public static void merge() {
+    /**
+     * 找到链表的中间节点
+     * 也可以用
+     * ListNode slow = head;
+     * ListNode fast = head.next;
+     * while (fast != null && fast.next != null) 来判断
+     *
+     * @param head 链表头节点
+     * @return 链表中间节点
+     */
+    public static ListNode findMid(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+
+    /**
+     * 归并排序两个生序链表
+     *
+     * @param h1 链表1的头节点
+     * @param h2 链表2的头节点
+     * @return 排序后链表的头节点
+     */
+    public static ListNode merge(ListNode h1, ListNode h2) {
+
+        // 头节点，但不返回，返回头节点的next节点
+        ListNode head = new ListNode(-1);
+        ListNode cur = head;
+        ListNode cur1 = h1;
+        ListNode cur2 = h2;
+        while (cur1 != null && cur2 != null) {
+            if (cur1.val < cur2.val) {
+                cur.next = cur1;
+                cur1 = cur1.next;
+            } else {
+                cur.next = cur2;
+                cur2 = cur2.next;
+            }
+            cur = cur.next;
+        }
+
+        if (cur1 != null) {
+            cur.next = cur1;
+        } else {
+            cur.next = cur2;
+        }
+
+        return head.next;
     }
 
 
 }
+
+
+
+
+
+
+
+
+
