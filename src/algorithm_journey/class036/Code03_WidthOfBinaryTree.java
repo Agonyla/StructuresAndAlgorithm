da7262253f2651f913a11f576b9f0f0114a85b42
Agony <gcl1197134883@gmail.com>
2024-06-19 10:48:09 +0800

    二叉树的最大特殊宽度

---
+++ src/algorithm_journey/class036/Code03_WidthOfBinaryTree.java
@@ -0,0 +1,20 @@
+package algorithm_journey.class036;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/19 10:37
+ * @describe:
+ * @link:
+ */
+public class Code03_WidthOfBinaryTree {
+
+    // todo
+    //
+    // 二叉树的最大特殊宽度
+    //
+    // 也是宽度遍历
+    // 两个队列 nodeQueue 节点队列，idQueue 编号队列
+    // 当前节点编号为 i，左孩子节点 2*i，右孩子节点 2*i+1
+    // 在放入节点队列时，同时把编号放入编号队列
+    // 最后 最右编号 - 最左编号
+}
