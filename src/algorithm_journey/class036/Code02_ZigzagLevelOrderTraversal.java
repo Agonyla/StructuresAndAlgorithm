079eda6030e963e5764c0888dcb920318940f8b7
Agony <gcl1197134883@gmail.com>
2024-06-19 10:48:04 +0800

    二叉树的锯齿形层序遍历

---
+++ src/algorithm_journey/class036/Code02_ZigzagLevelOrderTraversal.java
@@ -0,0 +1,21 @@
+package algorithm_journey.class036;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/19 10:24
+ * @describe:
+ * @link:
+ */
+public class Code02_ZigzagLevelOrderTraversal {
+
+
+    // todo
+
+    // 二叉树的锯齿形层序遍历
+    //
+    // 和上题类似
+    // 在弹出前先收集数组中的元素
+    // 从左往右收集 -> 收集 l -> r-1
+    // 从右往左收集 -> 收集 r-1 -> l
+    // 用一个变量 reverse 记录收集的顺序
+}
