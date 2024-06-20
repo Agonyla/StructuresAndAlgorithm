8e39483f9315cd2426d3fee08da9a57a47140fb1
Agony <gcl1197134883@gmail.com>
2024-06-20 11:15:21 +0800

    二叉树先序序列化和反序列化

---
+++ src/algorithm_journey/class036/Code05_PreorderSerializeAndDeserialize.java
@@ -0,0 +1,22 @@
+package algorithm_journey.class036;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/20 10:53
+ * @describe:
+ * @link:
+ */
+public class Code05_PreorderSerializeAndDeserialize {
+
+
+    public static void main(String[] args) {
+
+     
+    }
+
+    // todo
+
+    // 二叉树先序序列化和反序列化
+
+
+}
