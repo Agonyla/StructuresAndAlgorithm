098a059262b564940fb843df5be16633a8b13eaf
Agony <gcl1197134883@gmail.com>
2024-06-20 15:07:10 +0800

    验证完全二叉树

---
+++ src/algorithm_journey/class036/Code08_CompletenessOfBinaryTree.java
@@ -0,0 +1,20 @@
+package algorithm_journey.class036;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/20 14:19
+ * @describe:
+ * @link:
+ */
+public class Code08_CompletenessOfBinaryTree {
+
+
+    // todo
+
+    // 验证完全二叉树
+    //
+    // 思路
+    // BFS
+    // 1. 有右无左 -> false
+    // 2. 若是碰到有孩子不全的节点，那么接下来所有的节点都不必须是叶节点(没有孩子的节点)
+}
