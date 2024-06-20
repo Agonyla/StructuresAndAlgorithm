dfc15bf8214deb11fa5c40845df0d2e6b96c227b
Agony <gcl1197134883@gmail.com>
2024-06-20 11:15:14 +0800

    求二叉树的最大、最小深度

---
+++ src/algorithm_journey/class036/Code04_DepthOfBinaryTree.java
@@ -0,0 +1,18 @@
+package algorithm_journey.class036;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/20 10:44
+ * @describe:
+ * @link:
+ */
+public class Code04_DepthOfBinaryTree {
+
+
+    // todo
+
+    // 求二叉树的最大、最小深度
+    //
+    // 最大深度，递归求最大深度直接返回
+    // 最小深度，该节点有叶节点存在就算有高度，直到没有叶节点了才停止
+}
