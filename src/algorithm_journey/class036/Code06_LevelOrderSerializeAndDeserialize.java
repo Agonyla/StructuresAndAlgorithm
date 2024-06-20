0849744a96b020c4e26b7091474a7a0b0fc5b022
Agony <gcl1197134883@gmail.com>
2024-06-20 11:15:28 +0800

    二叉树按层序列化和反序列化

---
+++ src/algorithm_journey/class036/Code06_LevelOrderSerializeAndDeserialize.java
@@ -0,0 +1,14 @@
+package algorithm_journey.class036;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/20 11:09
+ * @describe:
+ * @link:
+ */
+public class Code06_LevelOrderSerializeAndDeserialize {
+
+    // todo
+
+    // 二叉树按层序列化和反序列化
+}
