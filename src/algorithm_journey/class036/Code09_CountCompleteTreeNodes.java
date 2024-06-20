a03ef078f38573036c1506ad6f7bb384488f3071
Agony <gcl1197134883@gmail.com>
2024-06-20 15:07:19 +0800

    求完全二叉树的节点个数

---
+++ src/algorithm_journey/class036/Code09_CountCompleteTreeNodes.java
@@ -0,0 +1,23 @@
+package algorithm_journey.class036;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/20 14:48
+ * @describe:
+ * @link:
+ */
+public class Code09_CountCompleteTreeNodes {
+
+    // todo
+
+    // 求完全二叉树的节点个数
+    //
+    // 思路
+    // 递归
+    // 左数的左边界求出总高度
+    // 右树左边界能不能找扎到最底，能 -> 左数是满树。
+    // 右侧递归调用
+    //
+    // 不能 -> 右树是满的 高度 -1
+    // 左侧递归调用
+}
