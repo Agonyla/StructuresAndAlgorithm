7a8723ad19fa36dc18628617db30f485c3253f64
Agony <gcl1197134883@gmail.com>
2024-06-21 14:38:33 +0800

    普通二叉树上寻找两个节点的最近公共祖先

---
+++ src/algorithm_journey/class037/Code01_LowestCommonAncestor.java
@@ -0,0 +1,21 @@
+package algorithm_journey.class037;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/21 14:05
+ * @describe:
+ * @link:
+ */
+public class Code01_LowestCommonAncestor {
+
+
+    // todo
+
+    // 普通二叉树上寻找两个节点的最近公共祖先
+    //
+    // q，p 两个节点
+    // 1. 两个节点存在包含关系
+    // -> 遇到其中一个节点就直接返回，不需要往下递归了
+    // 2. 两个节点属于两棵树
+    // -> 当一个节点 它的左树返回了 p，右树返回了 q，那么就直接返回该节点
+}
