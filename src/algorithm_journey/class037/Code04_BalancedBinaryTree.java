e753537ef59461865b02d6d10cedd40dcd80b276
Agony <gcl1197134883@gmail.com>
2024-06-25 14:41:56 +0800

    验证平衡二叉树

---
+++ src/algorithm_journey/class037/Code04_BalancedBinaryTree.java
@@ -0,0 +1,18 @@
+package algorithm_journey.class037;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/25 11:34
+ * @describe:
+ * @link:
+ */
+public class Code04_BalancedBinaryTree {
+
+
+    // todo
+
+    // 验证平衡二叉树
+    // 平衡二叉树 -> 对于每一个节点 左树的高度 和 右树的高度 相差不超过 1
+    // 思路
+    // 递归判断
+}
