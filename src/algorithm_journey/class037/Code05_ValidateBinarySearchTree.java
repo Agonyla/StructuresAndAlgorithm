e59678460aa141b6d4e2ac4e6f176fc3217f3053
Agony <gcl1197134883@gmail.com>
2024-06-25 14:42:12 +0800

    验证搜索二叉树

---
+++ src/algorithm_journey/class037/Code05_ValidateBinarySearchTree.java
@@ -0,0 +1,23 @@
+package algorithm_journey.class037;
+
+/**
+ * @author: Agony
+ * @create: 2024/6/25 14:14
+ * @describe:
+ * @link:
+ */
+public class Code05_ValidateBinarySearchTree {
+
+    // todo
+    //
+    // 验证搜索二叉树
+    //
+    // 思路
+    // 中序遍历一直是升序的话，就是搜索二叉树
+    // 当递归来到 null 时，下面的操作是为了不让空节点影响判断
+    // if (head == null) {
+    //      min = Long.MAX_VALUE;
+    //      max = Long.MIN_VALUE;
+    //      return true;
+    // }
+}
