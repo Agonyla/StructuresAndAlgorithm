ce660081e866a0061630bb0bbc014d4fd79b081e
Agony <gcl1197134883@gmail.com>
2024-06-27 10:09:45 +0800

    平衡二叉树

--- src/algorithm_journey/class037/Code04_BalancedBinaryTree.java
+++ src/algorithm_journey/class037/Code04_BalancedBinaryTree.java
@@ -1,12 +1,22 @@
 package algorithm_journey.class037;
 
+import algorithm_journey.utils.MathUtils.TreeNode;
+
 /**
+ * 平衡二叉树
+ *
  * @author: Agony
  * @create: 2024/6/25 11:34
- * @describe:
- * @link:
+ * @describe: 给定一个二叉树，判断它是否是
+ * 平衡二叉树
+ * @link: <a href="https://leetcode.cn/problems/balanced-binary-tree/description/">平衡二叉树</a>
  */
 public class Code04_BalancedBinaryTree {
+
+
+    public static void main(String[] args) {
+
+    }
 
 
     // todo
@@ -15,4 +25,14 @@ public class Code04_BalancedBinaryTree {
     // 平衡二叉树 -> 对于每一个节点 左树的高度 和 右树的高度 相差不超过 1
     // 思路
     // 递归判断
+
+    /**
+     * 验证平衡二叉树
+     *
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        return false;
+    }
 }
