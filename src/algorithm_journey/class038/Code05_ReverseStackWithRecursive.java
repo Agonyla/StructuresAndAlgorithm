f20c0cc876bb550114ac65e17806f93aa8abfd6c
Agony <gcl1197134883@gmail.com>
2024-07-01 20:50:03 +0800

    用递归函数逆序栈

---
+++ src/algorithm_journey/class038/Code05_ReverseStackWithRecursive.java
@@ -0,0 +1,20 @@
+package algorithm_journey.class038;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/1 20:04
+ * @describe:
+ * @link:
+ */
+public class Code05_ReverseStackWithRecursive {
+
+    // todo
+
+    // 用递归函数逆序栈
+    // 熟练掌握 Code05, Code06 对递归的熟悉程度会有质的飞跃
+
+    // 实现 bottomOut(Stack<Integer> stack) -> 返回栈底的元素，上面的元素压下来
+    // 用递归函数自己带空间，先取出来，再放进去
+
+    // reverse 函数， 调用 bottomOut 拿出栈底的元素，然后调用自己，再把拿出来的元素放进去
+}
