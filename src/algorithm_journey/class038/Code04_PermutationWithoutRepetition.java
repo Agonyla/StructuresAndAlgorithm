d107b114e2b5e73e916828f6ab0a44d012aefb78
Agony <gcl1197134883@gmail.com>
2024-07-01 20:49:57 +0800

    有重复项数组的去重全排列

---
+++ src/algorithm_journey/class038/Code04_PermutationWithoutRepetition.java
@@ -0,0 +1,17 @@
+package algorithm_journey.class038;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/1 19:56
+ * @describe:
+ * @link:
+ */
+public class Code04_PermutationWithoutRepetition {
+
+    // todo
+
+    // 有重复项数组的去重全排列
+
+    // 实现和上一题一样
+    // 多准备一个 hashset 用来去重
+}
