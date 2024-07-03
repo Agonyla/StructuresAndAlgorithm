5a5a9bc5cbff8bb7650c06b77cdf9b8040a68715
Agony <gcl1197134883@gmail.com>
2024-07-03 16:25:02 +0800

    用递归函数排序栈

--- src/algorithm_journey/class038/Code06_SortStackWithRecursive.java
+++ src/algorithm_journey/class038/Code06_SortStackWithRecursive.java
@@ -1,5 +1,7 @@
 package algorithm_journey.class038;
 
+import java.util.Stack;
+
 /**
  * @author: Agony
  * @create: 2024/7/1 20:16
@@ -22,4 +24,51 @@ public class Code06_SortStackWithRecursive {
     // d(depth - times, max, newTimes) -> 让新的所有的最大值撑底部, 其他数字相对次序不变
     // ...
     // 循环执行，指导 depth = 0
+
+
+    /**
+     * 统计栈的深度
+     * <p>
+     * 先把栈顶弹出
+     * 调用下层，获得下层深度
+     * 把栈顶压入
+     * 返回深度
+     *
+     * @param stack 栈
+     * @return 返回栈的深度
+     */
+    public static int a(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return 0;
+        }
+        Integer top = stack.pop();
+        int depth = a(stack);
+        stack.push(top);
+        return depth + 1;
+    }
+
+
+    /**
+     * @param stack 栈
+     * @param depth 栈的深度
+     * @return 返回栈内元素的最大值
+     */
+    public static int b(Stack<Integer> stack, int depth) {
+        return 0;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
