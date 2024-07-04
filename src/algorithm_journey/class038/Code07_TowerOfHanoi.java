606d04e7f62a0c82feb57e0da9a56e9b9334fa9c
Agony <gcl1197134883@gmail.com>
2024-07-04 15:20:51 +0800

    打印n层汉诺塔问题的最优移动轨迹

--- src/algorithm_journey/class038/Code07_TowerOfHanoi.java
+++ src/algorithm_journey/class038/Code07_TowerOfHanoi.java
@@ -8,7 +8,10 @@ package algorithm_journey.class038;
  */
 public class Code07_TowerOfHanoi {
 
-    // todo
+
+    public static void main(String[] args) {
+        hanoi(3, "左", "右", "中");
+    }
 
     // 打印n层汉诺塔问题的最优移动轨迹
 
@@ -24,4 +27,24 @@ public class Code07_TowerOfHanoi {
     // 再把上面的 i-1 个元素 从 other 移到 to -> f(i-1, other, to, from)
 
     // 把底部上面的所有元素看成一个整体 -> 实现只有两个元素的移动
+
+
+    /**
+     * 汉诺塔移动顺序
+     * 把所有的元素从 from 移到 to
+     *
+     * @param i     一共 i 个元素
+     * @param from  初始位置
+     * @param to    目标位置
+     * @param other 其他位置
+     */
+    public static void hanoi(int i, String from, String to, String other) {
+        if (i == 0) {
+            return;
+        }
+        hanoi(i - 1, from, other, to);
+        System.out.println("第 " + i + " 个从 " + from + " 移动到 " + to);
+        hanoi(i - 1, other, to, from);
+    }
+
 }
