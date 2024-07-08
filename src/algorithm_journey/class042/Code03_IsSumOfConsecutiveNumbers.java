ae26765b63ab4944ca19461357adadfab52a6645
Agony <gcl1197134883@gmail.com>
2024-07-08 13:19:26 +0800

    判断一个数字是否是若干数量(数量>1)的连续正整数的和

---
+++ src/algorithm_journey/class042/Code03_IsSumOfConsecutiveNumbers.java
@@ -0,0 +1,17 @@
+package algorithm_journey.class042;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/8 11:41
+ * @describe:
+ * @link:
+ */
+public class Code03_IsSumOfConsecutiveNumbers {
+
+    // todo
+
+
+    // 判断一个数字是否是若干数量(数量>1)的连续正整数的和
+
+    //
+}
