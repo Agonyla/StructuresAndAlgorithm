0e83d786af49c7884fb43c906b01797798dc5c49
Agony <gcl1197134883@gmail.com>
2024-07-08 13:19:35 +0800

    要求只有一个长度>=2的回文子串，求所有长度为n的red字符串中好串的数量

---
+++ src/algorithm_journey/class042/Code04_RedPalindromeGoodStrings.java
@@ -0,0 +1,18 @@
+package algorithm_journey.class042;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/8 12:59
+ * @describe:
+ * @link:
+ */
+public class Code04_RedPalindromeGoodStrings {
+
+    // todo
+
+    // 要求只有一个长度>=2的回文子串，求所有长度为n的red字符串中好串的数量
+
+    // 思路
+    // 先用暴力方法拼出所有字符串
+    // 再遍历所有的字串去验证是否是好串
+}
