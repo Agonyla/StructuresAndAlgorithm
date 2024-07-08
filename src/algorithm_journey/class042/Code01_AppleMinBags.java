f99ec7611285cd4336246aaf7580f153990939c6
Agony <gcl1197134883@gmail.com>
2024-07-08 13:19:13 +0800

    使用规格8和规格6的袋子买苹果问题

---
+++ src/algorithm_journey/class042/Code01_AppleMinBags.java
@@ -0,0 +1,16 @@
+package algorithm_journey.class042;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/8 11:15
+ * @describe:
+ * @link:
+ */
+public class Code01_AppleMinBags {
+
+    // todo
+
+    // 有装下8个苹果的袋子、装下6个苹果的袋子，一定要保证买苹果时所有使用的袋子都装满
+    // 对于无法装满所有袋子的方案不予考虑，给定n个苹果，返回至少要多少个袋子
+    // 如果不存在每个袋子都装满的方案返回-1
+}
