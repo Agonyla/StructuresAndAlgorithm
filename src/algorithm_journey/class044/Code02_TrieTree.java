45f051ca3ec0ea271e51a020005fd88b4a86c490
Agony <gcl1197134883@gmail.com>
2024-07-10 21:40:48 +0800

    静态数组实现前缀树

---
+++ src/algorithm_journey/class044/Code02_TrieTree.java
@@ -0,0 +1,16 @@
+package algorithm_journey.class044;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/10 10:05
+ * @describe:
+ * @link:
+ */
+public class Code02_TrieTree {
+
+    // todo
+
+    // 静态数组实现前缀树
+
+    // 
+}
