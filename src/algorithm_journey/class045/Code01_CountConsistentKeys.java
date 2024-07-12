fd3a2e7cd85e78e331ec9c8af1b65a2aa316b24a
Agony <gcl1197134883@gmail.com>
2024-07-12 11:14:15 +0800

    接头密匙

---
+++ src/algorithm_journey/class045/Code01_CountConsistentKeys.java
@@ -0,0 +1,18 @@
+package algorithm_journey.class045;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/12 10:04
+ * @describe:
+ * @link:
+ */
+public class Code01_CountConsistentKeys {
+
+    // todo
+
+    // 接头密匙
+    // 思路
+    // 把a组数字序列特征转化字符串，再转成成前缀树
+    // 如一组序列为 [3, 6, 50, 10] -> "3#44#-40#" -> 加到前缀树中
+    // 再用b组转成字符串去前缀树中匹配
+}
