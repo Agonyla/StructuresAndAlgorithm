88f7c2fb9709989fe45974b3429cae1b17bcc10f
Agony <gcl1197134883@gmail.com>
2024-07-17 11:21:15 +0800

    返回无序数组中正数和负数个数相等的最长子数组长度

---
+++ src/algorithm_journey/class046/Code04_PositivesEqualsNegtivesLongestSubarray.java
@@ -0,0 +1,20 @@
+package algorithm_journey.class046;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/17 09:32
+ * @describe:
+ * @link:
+ */
+public class Code04_PositivesEqualsNegtivesLongestSubarray {
+
+
+    // todo
+
+    // 返回无序数组中正数和负数个数相等的最长子数组长度
+
+    // 思路
+    // 和题目2类似
+    // 把正数转化为1，0维持不变，负数转化为-1
+    // 即求aim=0的最长子数组长度
+}
