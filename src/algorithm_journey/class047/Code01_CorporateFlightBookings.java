2812804edd907c3ff8cc2f88404c924ed3fbb734
Agony <gcl1197134883@gmail.com>
2024-07-18 12:07:59 +0800

    航班预订统计

---
+++ src/algorithm_journey/class047/Code01_CorporateFlightBookings.java
@@ -0,0 +1,15 @@
+package algorithm_journey.class047;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/18 11:44
+ * @describe:
+ * @link:
+ */
+public class Code01_CorporateFlightBookings {
+
+    // todo
+
+    // 航班预订统计
+
+}
