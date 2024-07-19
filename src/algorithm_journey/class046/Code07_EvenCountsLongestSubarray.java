a7325f32aee8228646ab0964e1b240da392370f5
Agony <gcl1197134883@gmail.com>
2024-07-19 20:45:54 +0800

    每个元音包含偶数次的最长子字符串

--- src/algorithm_journey/class046/Code07_EvenCountsLongestSubarray.java
+++ src/algorithm_journey/class046/Code07_EvenCountsLongestSubarray.java
@@ -1,14 +1,32 @@
 package algorithm_journey.class046;
 
+import java.util.Arrays;
+
 /**
+ * 每个元音包含偶数次的最长子字符串
+ *
  * @author: Agony
  * @create: 2024/7/17 10:48
- * @describe:
- * @link:
+ * @describe: 给你一个字符串 s ，请你返回满足以下条件的最长子字符串的长度：每个元音字母，
+ * 即 'a'，'e'，'i'，'o'，'u' ，在子字符串中都恰好出现了偶数次。
+ * <p>
+ * 输入：s = "eleetminicoworoep"
+ * 输出：13
+ * 解释：最长子字符串是 "leetminicowor" ，它包含 e，i，o 各 2 个，以及 0 个 a，u 。
+ * @link: <a href="https://leetcode.cn/problems/find-the-longest-substring-containing-vowels-in-even-counts/description/">每个元音包含偶数次的最长子字符串</a>
  */
 public class Code07_EvenCountsLongestSubarray {
 
-    // todo
+
+    public static void main(String[] args) {
+        String s = "eleetminicoworoep";
+        System.out.println(findTheLongestSubstring(s));
+
+
+        s = "bcbcbc";
+        System.out.println(findTheLongestSubstring(s));
+
+    }
 
     // 每个元音包含偶数次的最长子字符串
 
@@ -29,4 +47,63 @@ public class Code07_EvenCountsLongestSubarray {
     // 如果重复出现了这个状态，说明之前已经出现过了该状态，那么在之前出现的状态到i位置这段字符串内，aeiou 出现的次数是均是偶数次
     // 就直接返回 i-map[status]
     // 然后比较各个0到当前位置的最大值返回
+
+
+    /**
+     * 返回每个元音包含偶数次的最长子字符串
+     *
+     * @param s 字符串
+     * @return 每个元音包含偶数次的最长子字符串
+     */
+    public static int findTheLongestSubstring(String s) {
+
+        // 记录 uoiea 00000～11111 这32个状态的奇偶数量
+        int[] map = new int[32];
+        Arrays.fill(map, -2);
+        map[0] = -1;
+
+        int ans = 0;
+        // 0~i uoiea 出现的状态
+        int status = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int path = path(s.charAt(i));
+            if (path != -1) {
+                status ^= 1 << path;
+            }
+            if (map[status] != -2) {
+                ans = Math.max(ans, i - map[status]);
+            } else {
+                map[status] = i;
+            }
+        }
+        return ans;
+    }
+
+
+    /**
+     * 根据 uoiea 5、4、3、2、1
+     *
+     * @param cha
+     * @return
+     */
+    public static int path(char cha) {
+        return switch (cha) {
+            case 'a' -> 0;
+            case 'e' -> 1;
+            case 'i' -> 2;
+            case 'o' -> 3;
+            case 'u' -> 4;
+            default -> -1;
+        };
+    }
+
+
 }
+
+
+
+
+
+
+
