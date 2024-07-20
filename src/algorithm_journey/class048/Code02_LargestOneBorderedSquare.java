a63cee0512a4fa3b3c3af52b569dd0925035e5ef
Agony <gcl1197134883@gmail.com>
2024-07-20 15:40:32 +0800

    边框为1的最大正方形

---
+++ src/algorithm_journey/class048/Code02_LargestOneBorderedSquare.java
@@ -0,0 +1,20 @@
+package algorithm_journey.class048;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/20 15:19
+ * @describe:
+ * @link:
+ */
+public class Code02_LargestOneBorderedSquare {
+
+    // todo
+
+    // 边框为1的最大正方形
+
+    // 思路
+    // 行遍历、列遍历、最后再边长为1、2...直到行<n,列<m，去找最长边长的正方形
+    // 怎么判断是合法的正方形
+    // 整个面积的累加和 - 里面面积的累加和 = 正方形的边长
+    //
+}
