c61639d87879cbf2289d5f031f5831b966e6a028
Agony <gcl1197134883@gmail.com>
2024-07-22 23:04:18 +0800

    边框为1的最大正方形

--- src/algorithm_journey/class048/Code02_LargestOneBorderedSquare.java
+++ src/algorithm_journey/class048/Code02_LargestOneBorderedSquare.java
@@ -6,11 +6,22 @@ package algorithm_journey.class048;
  * @author: Agony
  * @create: 2024/7/20 15:19
  * @describe: 给你一个由若干 0 和 1 组成的二维网格 grid，请你找出边界全部由 1 组成的最大 正方形 子网格，并返回该子网格中的元素数量。如果不存在，则返回 0。
+ * 输入：grid = [[1,1,1],[1,0,1],[1,1,1]]
+ * 输出：9
  * @link: <a href="https://leetcode.cn/problems/largest-1-bordered-square/description/">最大的以 1 为边界的正方形</a>
  */
 public class Code02_LargestOneBorderedSquare {
 
-    // todo 还没做完，验证一下
+
+    public static void main(String[] args) {
+
+        int[][] grid = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
+        System.out.println(largest1BorderedSquare(grid));
+    }
 
     // 边框为1的最大正方形
 
@@ -24,6 +35,9 @@ public class Code02_LargestOneBorderedSquare {
 
     /**
      * 求边框为1的最大正方形
+     * 打败比例不高，但完全是常数时间的问题
+     * 时间复杂度O(n * m * min(n,m))，额外空间复杂度O(1)
+     * 复杂度指标上绝对是最优解
      *
      * @param grid 网格
      * @return 返回子网格内的元素数量（面积）
@@ -46,6 +60,7 @@ public class Code02_LargestOneBorderedSquare {
                 // 右下角 c,d
                 // side：边长
                 for (int c = a + ans, d = b + ans, side = ans + 1; c < row && d < col; c++, d++, side++) {
+                    // 整个面积的累加和 - 里面面积的累加和 = 正方形的边长
                     if (getSum(a, b, c, d, grid) - getSum(a + 1, b + 1, c - 1, d - 1, grid) == (side - 1) << 2) {
                         ans = side;
                     }
@@ -68,7 +83,9 @@ public class Code02_LargestOneBorderedSquare {
      * @return
      */
     public static int getSum(int a, int b, int c, int d, int[][] grid) {
-        return grid[c][d] - grid[a - 1][d] - grid[c][b - 1] + grid[a - 1][b - 1];
+
+        // 如边长=2，那么在在求里面面积的时候会出出现 a>c 的情况，这时候直接返回 0 就行
+        return a > c ? 0 : (grid[c][d] - get(a - 1, d, grid) - get(c, b - 1, grid) + get(a - 1, b - 1, grid));
     }
 
 
@@ -81,23 +98,23 @@ public class Code02_LargestOneBorderedSquare {
      */
     public static void build(int row, int col, int[][] grid) {
 
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 grid[i][j] += get(i, j - 1, grid) + get(i - 1, j, grid) - get(i - 1, j - 1, grid);
             }
         }
     }
 
     /**
-     * 返回该位置的前缀和
+     * 边界判断
      *
      * @param i    第i行
      * @param j    第j列
      * @param grid 数组
-     * @return 该位置的前缀和
+     * @return 无越界的值
      */
     public static int get(int i, int j, int[][] grid) {
-        return (i == 0 || j == 0) ? 0 : grid[i][j];
+        return (i < 0 || j < 0) ? 0 : grid[i][j];
     }
 
 
