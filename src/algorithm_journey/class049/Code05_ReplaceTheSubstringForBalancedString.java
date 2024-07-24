aebf687bd53c4ad7fe46e01e15b5f8365aed9cc6
Agony <gcl1197134883@gmail.com>
2024-07-24 12:22:11 +0800

    替换子串得到平衡字符串

---
+++ src/algorithm_journey/class049/Code05_ReplaceTheSubstringForBalancedString.java
@@ -0,0 +1,30 @@
+package algorithm_journey.class049;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/24 11:09
+ * @describe:
+ * @link:
+ */
+public class Code05_ReplaceTheSubstringForBalancedString {
+
+    // todo
+
+    // 替换子串得到平衡字符串
+
+    // 思路：
+
+    // 先把字符串转换成对应的数组 int[] arr = new int[s.length]
+    // 'Q'->0; 'W'->1; 'E'->2; 'R'->3
+    // 再用 int[] cnts = new int[4]
+    // 统计字符串中每一种字符出现的频次
+
+    // 实现 boolean ok(int cnts, int length, int require)
+    // return -> 能不能实现让整体字符串能达到要求
+    // cnts -> 除去l，r范围内的字符，即在窗口之外，每一种字符的词频统计
+    // length -> 窗口的长度
+    // require -> 要求：每一种字符都达到 n/4
+
+    // 总体流程：
+    // todo
+}
