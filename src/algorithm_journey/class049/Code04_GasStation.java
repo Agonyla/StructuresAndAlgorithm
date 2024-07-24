1a6044c132db966e91266b4612b8382ab729f609
Agony <gcl1197134883@gmail.com>
2024-07-24 10:00:04 +0800

    加油站

---
+++ src/algorithm_journey/class049/Code04_GasStation.java
@@ -0,0 +1,16 @@
+package algorithm_journey.class049;
+
+/**
+ * @author: Agony
+ * @create: 2024/7/24 9:58
+ * @describe:
+ * @link:
+ */
+public class Code04_GasStation {
+
+    // todo
+
+    // 加油站
+    // 以上题目都是用结尾情况下的答案
+    // 该题是用开头讨论
+}
