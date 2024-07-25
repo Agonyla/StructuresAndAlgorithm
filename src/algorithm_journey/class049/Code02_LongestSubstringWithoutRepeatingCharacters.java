f131c45d4d2babbb8018fc773396b58e5bb80d67
Agony <gcl1197134883@gmail.com>
2024-07-25 20:51:32 +0800

    无重复字符的最长子串

--- src/algorithm_journey/class049/Code02_LongestSubstringWithoutRepeatingCharacters.java
+++ src/algorithm_journey/class049/Code02_LongestSubstringWithoutRepeatingCharacters.java
@@ -1,15 +1,29 @@
 package algorithm_journey.class049;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
+ * 无重复字符的最长子串
+ *
  * @author: Agony
  * @create: 2024/7/22 18:07
- * @describe:
- * @link:
+ * @describe: 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串的长度。
+ * <p>
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * @link: <a href="https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/">无重复字符的最长子串</a>
  */
 public class Code02_LongestSubstringWithoutRepeatingCharacters {
 
-    // todo
 
+    public static void main(String[] args) {
+
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+
+    }
     // 无重复字符的最长子串
 
     // 思路：
@@ -29,5 +43,69 @@ public class Code02_LongestSubstringWithoutRepeatingCharacters {
     // last[cha] 就表示 cha 字符 上次出现的位置
     // 如：last['a'] = 3，表示字符 'a' 上次出现的位置为 3
     // 记得更新最新位置
-    // todo 感觉用 map 记录位置也行，可以试一试
+
+
+    /**
+     * 无重复字符的最长子串
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring(String s) {
+
+        char[] chas = s.toCharArray();
+
+        // 记录字符上一次出现的位置
+        int[] map = new int[256];
+        Arrays.fill(map, -1);
+        int ans = 0;
+        for (int r = 0, l = 0; r < s.length(); r++) {
+
+            // 右边界遇到字符。左边界更新
+            // 如果该字符之前还没遇到过 -> 保持原位置
+            // 如果该字符之前遇到过但是在l之前 -> 保持原位置
+            // 如果该字符之前遇到过但是在l之后 -> 来到这个字符+1的位置
+            l = Math.max(map[chas[r]] + 1, l);
+
+            // ans 更新
+            ans = Math.max(ans, r - l + 1);
+
+            // 把该字符加入到map中
+            char cha = chas[r];
+            map[cha] = r;
+        }
+        return ans;
+    }
+
+
+    /**
+     * 无重复字符的最长子串
+     * map实现
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstringByMap(String s) {
+
+        char[] chas = s.toCharArray();
+
+        // 记录字符上一次出现的位置
+        HashMap<Character, Integer> map = new HashMap<>();
+        int ans = 0;
+        for (int r = 0, l = 0; r < s.length(); r++) {
+
+            // 右边界遇到字符。左边界更新
+            // 如果该字符之前还没遇到过 -> 保持原位置
+            // 如果该字符之前遇到过但是在l之前 -> 保持原位置
+            // 如果该字符之前遇到过但是在l之后 -> 来到这个字符+1的位置
+            l = Math.max(map.getOrDefault(chas[r], -1) + 1, l);
+
+            // ans 更新
+            ans = Math.max(ans, r - l + 1);
+
+            // 把该字符加入到map中
+            map.put(chas[r], r);
+        }
+        return ans;
+    }
 }
