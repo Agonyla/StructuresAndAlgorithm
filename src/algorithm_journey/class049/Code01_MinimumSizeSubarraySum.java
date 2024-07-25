9f1c5de916f7cd95f3f23d3b72022d38078536ca
Agony <gcl1197134883@gmail.com>
2024-07-25 14:52:19 +0800

    累加和大于等于target的最短子数组长度

--- src/algorithm_journey/class049/Code01_MinimumSizeSubarraySum.java
+++ src/algorithm_journey/class049/Code01_MinimumSizeSubarraySum.java
@@ -1,10 +1,16 @@
 package algorithm_journey.class049;
 
 /**
+ * 长度最小的子数组
+ *
  * @author: Agony
  * @create: 2024/7/22 17:48
- * @describe:
- * @link:
+ * @describe: 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ * <p>
+ * 找出该数组中满足其总和大于等于 target 的长度最小的
+ * 子数组
+ * [nums_l, nums_l+1, ..., nums_r-1, nums_r] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ * @link: <a href="https://leetcode.cn/problems/minimum-size-subarray-sum/description/">长度最小的子数组</a>
  */
 public class Code01_MinimumSizeSubarraySum {
 
@@ -21,4 +27,18 @@ public class Code01_MinimumSizeSubarraySum {
     // 不满足右边界++，扩大窗口
     // 。。。
     // 求最小值返回
+
+
+    /**
+     * 长度最小的子数组
+     *
+     * @param target 目标和
+     * @param nums   数组
+     * @return 返回长度最小的子数组长度
+     */
+    public static int minSubArrayLen(int target, int[] nums) {
+
+
+        return 0;
+    }
 }
