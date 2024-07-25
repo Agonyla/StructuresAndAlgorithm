2d313c12a30c2cde3e55df70473c95b24e206aad
Agony <gcl1197134883@gmail.com>
2024-07-25 20:52:00 +0800

    加油站

--- src/algorithm_journey/class049/Code04_GasStation.java
+++ src/algorithm_journey/class049/Code04_GasStation.java
@@ -1,10 +1,16 @@
 package algorithm_journey.class049;
 
 /**
+ * 加油站
+ *
  * @author: Agony
  * @create: 2024/7/24 9:58
- * @describe:
- * @link:
+ * @describe: 在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
+ * <p>
+ * 你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。你从其中的一个加油站出发，开始时油箱为空。
+ * <p>
+ * 给定两个整数数组 gas 和 cost ，如果你可以按顺序绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1 。如果存在解，则 保证 它是 唯一 的。
+ * @link: <a href="https://leetcode.cn/problems/gas-station/description/">加油站</a>
  */
 public class Code04_GasStation {
 
@@ -28,5 +34,16 @@ public class Code04_GasStation {
     // sum一直>=0，右窗口不断向右扩
     // 直到窗口长度等于数组长度
 
+
+    /**
+     * 加油站
+     *
+     * @param gas  油数组
+     * @param cost 消耗数组
+     * @return
+     */
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        return 0;
+    }
 
 }
