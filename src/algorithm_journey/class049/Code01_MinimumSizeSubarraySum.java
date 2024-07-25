3f21d5a6c8290f8ccc0ef8d0cfc861d15e844a31
Agony <gcl1197134883@gmail.com>
2024-07-25 20:51:24 +0800

    累加和大于等于target的最短子数组长度

--- src/algorithm_journey/class049/Code01_MinimumSizeSubarraySum.java
+++ src/algorithm_journey/class049/Code01_MinimumSizeSubarraySum.java
@@ -10,11 +10,21 @@ package algorithm_journey.class049;
  * 找出该数组中满足其总和大于等于 target 的长度最小的
  * 子数组
  * [nums_l, nums_l+1, ..., nums_r-1, nums_r] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ * <p>
+ * 示例：
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
  * @link: <a href="https://leetcode.cn/problems/minimum-size-subarray-sum/description/">长度最小的子数组</a>
  */
 public class Code01_MinimumSizeSubarraySum {
 
-    // todo
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 3, 1, 2, 4, 3};
+        System.out.println(minSubArrayLen(7, nums));
+    }
 
     // 累加和大于等于target的最短子数组长度
 
@@ -31,6 +41,7 @@ public class Code01_MinimumSizeSubarraySum {
 
     /**
      * 长度最小的子数组
+     * 求子数组在每个位置 结尾 情况下的答案
      *
      * @param target 目标和
      * @param nums   数组
@@ -39,6 +50,22 @@ public class Code01_MinimumSizeSubarraySum {
     public static int minSubArrayLen(int target, int[] nums) {
 
 
-        return 0;
+        int ans = Integer.MAX_VALUE;
+        int sum = 0;
+        // target = 7, nums = [2,3,1,2,4,3]
+        // 因为是求以r结尾的情况下的答案，
+        // 每次 sum += nums[r];
+        // 让左窗口尽量往右缩
+        for (int r = 0, l = 0; r < nums.length; r++) {
+            sum += nums[r];
+            while (sum - nums[l] >= target) {
+                sum -= nums[l++];
+            }
+
+            if (sum >= target) {
+                ans = Math.min(ans, r - l + 1);
+            }
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
