5a3965200bef5717ed5ea4d08e251c0e83de323a
Agony <gcl1197134883@gmail.com>
2024-08-21 18:08:55 +0800

    最长有效括号

--- src/algorithm_journey/class066/Code06_LongestValidParentheses.java
+++ src/algorithm_journey/class066/Code06_LongestValidParentheses.java
@@ -1,14 +1,31 @@
 package algorithm_journey.class066;
 
 /**
+ * 最长有效括号
+ *
  * @author: Agony
  * @create: 2024/7/27 11:52
- * @describe:
- * @link:
+ * @describe: 给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "(()"
+ * 输出：2
+ * 解释：最长有效括号子串是 "()"
+ * 示例 2：
+ * <p>
+ * 输入：s = ")()())"
+ * 输出：4
+ * 解释：最长有效括号子串是 "()()"
+ * @link: <a href="https://leetcode.cn/problems/longest-valid-parentheses/description/">最长有效括号</a>
  */
 public class Code06_LongestValidParentheses {
 
     // todo
+
+    public static void main(String[] args) {
+
+    }
 
     // 最长有效括号
 
@@ -25,4 +42,25 @@ public class Code06_LongestValidParentheses {
     // 因为dp[p-1]已经是其结尾的最长长度，它已经往左判断过了
     //
     // 然后返回 dp 中的最大值
+
+
+    /**
+     * 最长有效括号
+     *
+     * @param s
+     * @return
+     */
+    public static int longestValidParentheses(String s) {
+
+        return 0;
+    }
 }
+
+
+
+
+
+
+
+
+
