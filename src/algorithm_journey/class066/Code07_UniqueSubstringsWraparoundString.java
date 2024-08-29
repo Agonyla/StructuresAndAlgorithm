d2a2dfb55913d0dd9e567915274a8942d5d65ab2
Agony <gcl1197134883@gmail.com>
2024-08-29 14:13:27 +0800

    环绕字符串中唯一的子字符串

--- src/algorithm_journey/class066/Code07_UniqueSubstringsWraparoundString.java
+++ src/algorithm_journey/class066/Code07_UniqueSubstringsWraparoundString.java
@@ -13,7 +13,13 @@ package algorithm_journey.class066;
  */
 public class Code07_UniqueSubstringsWraparoundString {
 
-    // todo
+
+    public static void main(String[] args) {
+
+        System.out.println(findSubstringInWraproundString("a"));
+        System.out.println(findSubstringInWraproundString("cac"));
+        System.out.println(findSubstringInWraproundString("zab"));
+    }
 
     // 环绕字符串中唯一的子字符串
 
@@ -23,25 +29,76 @@ public class Code07_UniqueSubstringsWraparoundString {
     // dp表含义：int[] dp = new int[26]。
     // dp[0]表示以'a'结尾的字符串，它的最大延伸长度是多少
     // dp[25]表示以'z'结尾的字符串，它的最大延伸长度是多少
+    // 把dp表所有值累加就是答案
     //
     // 实现：
-    // 先把字符串转成 int[] s 数组 "abc" -> {0,1,2}
-    // dp[s[0]]=1 -> 表示第一个数只能延伸1位
+    // 先把字符串转成 int[] s 数组。如 "abc" -> {0,1,2}  关键 ‼️
+    // dp[s[0]]=1 -> 表示s字符串中以第1个字符结尾的子串只能延伸1位
+    // dp[s[4]]=5 -> 表示s字符串中以第5个字符结尾的子串只能延伸5位
     // 什么情况下能延伸
     // cur=s[i], pre=s[i-1]
-    // pre==25 && cur==0  || pre+1==cur
+    // if(pre==25 && cur==0  || pre+1==cur)
     // 或者  (cur-pre+25)%26==0 ??? 试一试行不行
-    // 更新最大长度
+    // length++
+    // 否则 length=1
+    // 更新最大长度 -> dp[s[i]]=length
     // 返回dp中的所有值累加和
 
 
     /**
      * 环绕字符串中唯一的子字符串
      *
-     * @param s
+     * @param str
      * @return
      */
-    public static int findSubstringInWraproundString(String s) {
-        return 0;
+    public static int findSubstringInWraproundString(String str) {
+
+        int n = str.length();
+        int[] s = new int[n];
+        int[] dp = new int[26];
+
+        // abad -> 0103
+        for (int i = 0; i < n; i++) {
+            s[i] = str.charAt(i) - 'a';
+        }
+
+        dp[s[0]] = 1;
+
+        for (int i = 1, cur, pre, length = 1; i < n; i++) {
+            cur = s[i];
+            pre = s[i - 1];
+            // (cur - pre + 25) % 26 == 0 这个也可以
+            if ((cur == 0 && pre == 25) || (cur == pre + 1)) {
+                length++;
+            } else {
+                length = 1;
+            }
+            dp[cur] = Math.max(dp[cur], length);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++) {
+            ans += dp[i];
+        }
+
+        return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
