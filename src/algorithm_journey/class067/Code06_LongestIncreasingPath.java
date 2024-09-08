d16661fe0ebd5a9f7aa4cae533d92d22ad5949c5
Agony <gcl1197134883@gmail.com>
2024-09-08 21:28:42 +0800

    矩阵中的最长递增路径

--- src/algorithm_journey/class067/Code06_LongestIncreasingPath.java
+++ src/algorithm_journey/class067/Code06_LongestIncreasingPath.java
@@ -13,8 +13,24 @@ package algorithm_journey.class067;
 public class Code06_LongestIncreasingPath {
 
 
-    // todo
+    public static void main(String[] args) {
 
+
+        int[][] matrix = {{9, 9, 4}, {6, 6, 8}, {2, 1, 1}};     // 4
+        System.out.println(longestIncreasingPath1(matrix));
+        System.out.println(longestIncreasingPath2(matrix));
+
+
+        matrix = new int[][]{{3, 4, 5}, {3, 2, 6}, {2, 2, 1}};  // 4
+        System.out.println(longestIncreasingPath1(matrix));
+        System.out.println(longestIncreasingPath2(matrix));
+
+
+        matrix = new int[][]{{1}};                              // 1
+        System.out.println(longestIncreasingPath1(matrix));
+        System.out.println(longestIncreasingPath2(matrix));
+
+    }
     // 矩阵中的最长递增路径
     //
     // 暴力递归
@@ -32,14 +48,112 @@ public class Code06_LongestIncreasingPath {
 
 
     /**
-     * 矩阵中的最长递增路径
+     * 矩阵中的最长递增路径 - 暴力递归
      *
      * @param matrix
      * @return
      */
-    public int longestIncreasingPath(int[][] matrix) {
+    public static int longestIncreasingPath1(int[][] matrix) {
 
+        int ans = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                ans = Math.max(f1(matrix, i, j), ans);
+            }
+        }
 
-        return 1;
+        return ans;
     }
+
+
+    /**
+     * 暴力递归
+     *
+     * @param matrix 矩阵
+     * @param i      当前来到i行
+     * @param j      当前来到j列
+     * @return 返回当前来到(i, j)位置能走出最长的路径
+     */
+    public static int f1(int[][] matrix, int i, int j) {
+
+
+        // up
+        int next = 0;
+        if (i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j]) {
+            next = Math.max(next, f1(matrix, i - 1, j));
+        }
+
+        // down
+        if (i + 1 < matrix.length && matrix[i + 1][j] > matrix[i][j]) {
+            next = Math.max(next, f1(matrix, i + 1, j));
+        }
+
+        // left
+        if (j - 1 >= 0 && matrix[i][j - 1] > matrix[i][j]) {
+            next = Math.max(next, f1(matrix, i, j - 1));
+        }
+
+        // right
+        if (j + 1 < matrix[0].length && matrix[i][j + 1] > matrix[i][j]) {
+            next = Math.max(next, f1(matrix, i, j + 1));
+        }
+        return next + 1;
+    }
+
+
+    /**
+     * 矩阵中的最长递增路径 - 记忆化搜索
+     *
+     * @param matrix
+     * @return
+     */
+    public static int longestIncreasingPath2(int[][] matrix) {
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dp = new int[n][m];
+        int ans = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                ans = Math.max(f2(matrix, i, j, dp), ans);
+            }
+        }
+        return ans;
+    }
+
+    public static int f2(int[][] matrix, int i, int j, int[][] dp) {
+
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+        // up
+        int next = 0;
+        if (i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j]) {
+            next = Math.max(next, f2(matrix, i - 1, j, dp));
+        }
+
+        // down
+        if (i + 1 < matrix.length && matrix[i + 1][j] > matrix[i][j]) {
+            next = Math.max(next, f2(matrix, i + 1, j, dp));
+        }
+
+        // left
+        if (j - 1 >= 0 && matrix[i][j - 1] > matrix[i][j]) {
+            next = Math.max(next, f2(matrix, i, j - 1, dp));
+        }
+
+        // right
+        if (j + 1 < matrix[0].length && matrix[i][j + 1] > matrix[i][j]) {
+            next = Math.max(next, f2(matrix, i, j + 1, dp));
+        }
+        dp[i][j] = next + 1;
+        return next + 1;
+    }
+
+
 }
+
+
+
+
+
