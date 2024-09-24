8283f2fef9aaacf4ef80eab9e0cfd18e74639080
Agony <gcl1197134883@gmail.com>
2024-09-24 15:41:40 +0800

    扰乱字符串

--- src/algorithm_journey/class069/Code05_ScrambleString.java
+++ src/algorithm_journey/class069/Code05_ScrambleString.java
@@ -1,10 +1,18 @@
 package algorithm_journey.class069;
 
 /**
+ * 扰乱字符串
+ *
  * @author: Agony
  * @create: 2024/9/11 11:50
- * @describe:
- * @link:
+ * @describe: 使用下面描述的算法可以扰乱字符串 s 得到字符串 t ：
+ * 如果字符串的长度为 1 ，算法停止
+ * 如果字符串的长度 > 1 ，执行下述步骤：
+ * 在一个随机下标处将字符串分割成两个非空的子字符串。即，如果已知字符串 s ，则可以将其分成两个子字符串 x 和 y ，且满足 s = x + y 。
+ * 随机 决定是要「交换两个子字符串」还是要「保持这两个子字符串的顺序不变」。即，在执行这一步骤之后，s 可能是 s = x + y 或者 s = y + x 。
+ * 在 x 和 y 这两个子字符串上继续从步骤 1 开始递归执行此算法。
+ * 给你两个 长度相等 的字符串 s1 和 s2，判断 s2 是否是 s1 的扰乱字符串。如果是，返回 true ；否则，返回 false 。
+ * @link: <a href="https://leetcode.cn/problems/scramble-string/description/">扰乱字符串</a>
  */
 public class Code05_ScrambleString {
 
@@ -57,6 +65,12 @@ public class Code05_ScrambleString {
     // 填表时要注意：
     // l1+len<=n
     // l2+len<=n
+
+
+    public static boolean isScramble(String s1, String s2) {
+
+        return false;
+    }
 
 
 }
