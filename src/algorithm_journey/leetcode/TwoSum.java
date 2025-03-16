83f3e5221d6570ff3f3ff254174b9170d84aa755
Agony <gcl1197134883@gmail.com>
2025-03-16 21:31:50 +0800

    两数之和

---
+++ src/algorithm_journey/leetcode/TwoSum.java
@@ -0,0 +1,33 @@
+package algorithm_journey.leetcode;
+
+import java.util.HashMap;
+
+/**
+ * @author: Agony
+ * @create: 2025/3/16 21:25
+ * @describe: 两数之和
+ * @link: <a href="https://leetcode.cn/problems/two-sum/description/">两数之和</a>
+ */
+public class TwoSum {
+
+
+    /**
+     * 两数之和
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+}
