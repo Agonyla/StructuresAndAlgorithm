20f37e36a99eb24cf0b9765ffb36d14d843074b5
Agony <gcl1197134883@gmail.com>
2025-04-03 22:40:48 +0800

    第k个排列

---
+++ src/algorithm_journey/hwod/KthPermutation.java
@@ -0,0 +1,14 @@
+package algorithm_journey.hwod;
+
+/**
+ * @author: Agony
+ * @create: 2025/4/3 22:37
+ * @describe: 第k个排列
+ */
+public class KthPermutation {
+
+    public static void main(String[] args) {
+
+
+    }
+}
