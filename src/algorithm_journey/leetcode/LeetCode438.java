cfb6228d351c4c15207c6baef94f91661af11f5d
Agony <gcl1197134883@gmail.com>
2025-04-04 21:22:08 +0800

    找到字符串中所有字母异位词

---
+++ src/algorithm_journey/leetcode/LeetCode438.java
@@ -0,0 +1,29 @@
+package algorithm_journey.leetcode;
+
+import java.util.List;
+
+/**
+ * @author: Agony
+ * @create: 2025/4/4 21:18
+ * @describe: 找到字符串中所有字母异位词
+ * <a href="https://leetcode.cn/problems/find-all-anagrams-in-a-string/description/?envType=company&envId=huawei&favoriteSlug=huawei-three-months">找到字符串中所有字母异位词</a>
+ */
+public class LeetCode438 {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static List<Integer> findAnagrams(String s, String p) {
+
+
+        // todo
+
+
+        return null;
+    }
+
+
+}
