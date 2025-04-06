aba757effe9e126b1d1f2f3940656b0d50818e3d
Agony <gcl1197134883@gmail.com>
2025-04-06 18:54:51 +0800

    面试题 16.26. 计算器

--- src/algorithm_journey/leetcode/LeetCodeCalculate.java
+++ src/algorithm_journey/leetcode/LeetCodeCalculate.java
@@ -14,56 +14,48 @@ public class LeetCodeCalculate {
     public static void main(String[] args) {
         String s = "3+2*2";
         System.out.println(calculate(s));
+
+        s = " 3+5 / 2 ";
+        System.out.println(calculate(s));
     }
 
 
-    // todo 删了自己重新敲一遍
-    
-
     public static int calculate(String s) {
-        // 移除所有空格
+
         s = s.replaceAll("\\s", "");
 
-        // 使用栈来保存中间结果
         Stack<Integer> stack = new Stack<>();
 
-        // 记录当前数字和当前操作符
-        int currentNumber = 0;
+        int ans = 0;
+        int currentNum = 0;
         char operation = '+';
-
         for (int i = 0; i < s.length(); i++) {
+
             char currentChar = s.charAt(i);
 
-            // 如果是数字，更新当前数字
             if (Character.isDigit(currentChar)) {
-                currentNumber = currentNumber * 10 + (currentChar - '0');
+                currentNum = currentNum * 10 + (currentChar - '0');
             }
 
-            // 如果是操作符或者到达字符串末尾，处理当前数字和操作符
             if (!Character.isDigit(currentChar) || i == s.length() - 1) {
-                // 根据上一个操作符处理当前数字
                 if (operation == '+') {
-                    stack.push(currentNumber);
+                    stack.push(currentNum);
                 } else if (operation == '-') {
-                    stack.push(-currentNumber);
+                    stack.push(-currentNum);
                 } else if (operation == '*') {
-                    stack.push(stack.pop() * currentNumber);
+                    stack.push(stack.pop() * currentNum);
                 } else if (operation == '/') {
-                    stack.push(stack.pop() / currentNumber);
+                    stack.push(stack.pop() / currentNum);
                 }
-
-                // 更新操作符和重置当前数字
+                currentNum = 0;
                 operation = currentChar;
-                currentNumber = 0;
             }
         }
 
-        // 计算最终结果
-        int result = 0;
         while (!stack.isEmpty()) {
-            result += stack.pop();
+            ans += stack.pop();
         }
 
-        return result;
+        return ans;
     }
 }
