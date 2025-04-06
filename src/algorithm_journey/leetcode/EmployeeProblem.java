078342c364e4f331814731a259753675209d3f39
Agony <gcl1197134883@gmail.com>
2025-04-06 23:11:32 +0800

    税务

--- src/algorithm_journey/leetcode/EmployeeProblem.java
+++ src/algorithm_journey/leetcode/EmployeeProblem.java
@@ -11,6 +11,8 @@ import java.util.List;
 
 
 public class EmployeeProblem {
+
+
     public static void main(String[] args) {
         List<Employee> employees = new ArrayList<>();
 
@@ -23,38 +25,73 @@ public class EmployeeProblem {
         employees.add(xj);
         employees.add(xh);
 
-        for (Employee employee : employees) {
+        // for (Employee employee : employees) {
+        //
+        //     double salary = employee.getSalary();
+        //     if (salary <= 3500) {
+        //         String format = String.format("%.1f", 0.0);
+        //         System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+        //     } else if (salary > 3500 && salary <= 4500) {
+        //         double pay = (salary - 3500) * 0.1 - 105;
+        //         String format = String.format("%.1f", pay);
+        //         System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+        //     } else if (salary > 4500 && salary <= 9000) {
+        //         double pay = (salary - 3500) * 0.2 - 555;
+        //         String format = String.format("%.1f", pay);
+        //         System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+        //     } else if (salary > 9000 && salary <= 35000) {
+        //         double pay = (salary - 3500) * 0.25 - 1055;
+        //         String format = String.format("%.1f", pay);
+        //         System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+        //     } else if (salary > 35000 && salary <= 55000) {
+        //         double pay = (salary - 3500) * 0.3 - 2755;
+        //         String format = String.format("%.1f", pay);
+        //         System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+        //     } else if (salary > 55000 && salary <= 80000) {
+        //         double pay = (salary - 3500) * 0.35 - 5505;
+        //         String format = String.format("%.1f", pay);
+        //         System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+        //     } else if (salary > 80000) {
+        //         double pay = (salary - 3500) * 0.45 - 13505;
+        //         String format = String.format("%.1f", pay);
+        //         System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+        //     }
+        // }
 
-            double salary = employee.getSalary();
-            if (salary <= 3500) {
-                String format = String.format("%.1f", 0.0);
-                System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
-            } else if (salary > 3500 && salary <= 4500) {
-                double pay = (salary - 3500) * 0.1 - 105;
-                String format = String.format("%.1f", pay);
-                System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
-            } else if (salary > 4500 && salary <= 9000) {
-                double pay = (salary - 3500) * 0.2 - 555;
-                String format = String.format("%.1f", pay);
-                System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
-            } else if (salary > 9000 && salary <= 35000) {
-                double pay = (salary - 3500) * 0.25 - 1055;
-                String format = String.format("%.1f", pay);
-                System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
-            } else if (salary > 35000 && salary <= 55000) {
-                double pay = (salary - 3500) * 0.3 - 2755;
-                String format = String.format("%.1f", pay);
-                System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
-            } else if (salary > 55000 && salary <= 80000) {
-                double pay = (salary - 3500) * 0.35 - 5505;
-                String format = String.format("%.1f", pay);
-                System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
-            } else if (salary > 80000) {
-                double pay = (salary - 3500) * 0.45 - 13505;
-                String format = String.format("%.1f", pay);
-                System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
-            }
-        }
+
+        employees.forEach(
+                employee -> {
+                    double salary = employee.getSalary();
+                    if (salary <= 3500) {
+                        String format = String.format("%.1f", 0.0);
+                        System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+                    } else if (salary > 3500 && salary <= 4500) {
+                        double pay = (salary - 3500) * 0.1 - 105;
+                        String format = String.format("%.1f", pay);
+                        System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+                    } else if (salary > 4500 && salary <= 9000) {
+                        double pay = (salary - 3500) * 0.2 - 555;
+                        String format = String.format("%.1f", pay);
+                        System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+                    } else if (salary > 9000 && salary <= 35000) {
+                        double pay = (salary - 3500) * 0.25 - 1055;
+                        String format = String.format("%.1f", pay);
+                        System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+                    } else if (salary > 35000 && salary <= 55000) {
+                        double pay = (salary - 3500) * 0.3 - 2755;
+                        String format = String.format("%.1f", pay);
+                        System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+                    } else if (salary > 55000 && salary <= 80000) {
+                        double pay = (salary - 3500) * 0.35 - 5505;
+                        String format = String.format("%.1f", pay);
+                        System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+                    } else if (salary > 80000) {
+                        double pay = (salary - 3500) * 0.45 - 13505;
+                        String format = String.format("%.1f", pay);
+                        System.out.println(employee.getName() + "应该缴纳的个人所得税是: " + format);
+                    }
+                }
+        );
 
     }
 }
