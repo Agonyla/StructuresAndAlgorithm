bb59a512c27abf0265f75dac5889982972922d74
Agony <gcl1197134883@gmail.com>
2025-04-07 12:47:38 +0800

    盛最多水的容器

--- src/algorithm_journey/leetcode/LeetCode11.java
+++ src/algorithm_journey/leetcode/LeetCode11.java
@@ -9,8 +9,7 @@ package algorithm_journey.leetcode;
 public class LeetCode11 {
 
     public static void main(String[] args) {
-
-
+        
         int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
         System.out.println(maxArea(height));
     }
