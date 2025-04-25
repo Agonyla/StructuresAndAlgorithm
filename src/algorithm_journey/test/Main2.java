4d23d7ad9dea01569673cefdc369a31da1a6fd3b
Agony <gcl1197134883@gmail.com>
2025-04-25 20:40:53 +0800

    test

--- src/algorithm_journey/test/Main2.java
+++ src/algorithm_journey/test/Main2.java
@@ -1,5 +1,8 @@
 package algorithm_journey.test;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @author: Agony
  * @create: 2025/4/24 19:17
@@ -9,6 +12,17 @@ public class Main2 {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        scanner.nextLine();
+
+        String[] arr = scanner.nextLine().split(" ");
+
+        // 按照长度生序，长度一样则按照字典序生序
+        Arrays.sort(arr, (a, b) -> a.length() == b.length() ? a.compareTo(b) : a.length() - b.length());
+        System.out.println(Arrays.toString(arr));
 
     }
 }
