ee02c40568f077b2ebf3758af093b15dac5b8a38
Agony <gcl1197134883@gmail.com>
2026-08-20 14:42:46 +0800

    Stream Practice6 1~5

--- src/practice/stream/StreamPractice6.java
+++ src/practice/stream/StreamPractice6.java
@@ -1,7 +1,10 @@
 package practice.stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author: Agony
@@ -85,14 +88,19 @@ public class StreamPractice6 {
         );
 
         // 1. 找出：研发部员工，工资大于 15000，按工资从高到低排序，最终只返回员工姓名
+        System.out.println(t1(employees));
         // 2. 从所有员工中找出工资最高的 3 名员工。工资降序，如果工资相同，年龄小的排前面
+        System.out.println(t2(employees));
         // 3. 将员工按照部门分组，并统计每个部门有多少人。
+        System.out.println(t3(employees));
         // 4. 按照部门统计平均工资。
+        System.out.println(t4(employees));
         // 5. 按照部门分组，然后找出每个部门工资最高的员工
+        System.out.println(t5(employees));
         // 6. 定义三个年龄段：30岁以下、30~34岁、35岁及以上 按照年龄段对员工分组。
         // 7. 统计全部员工的技能出现次数。
         // 8. 找出出现次数最多的技能，在第 7 题基础上，进一步找出：被最多员工掌握的技能
-        // 9.找出掌握 Java 但不会 Spring/SpringBoot 的员工
+        // 9. 找出掌握 Java 但不会 Spring/SpringBoot 的员工
         // 10. 部门工资总额排名
         // 11. 判断部门是否存在“高薪员工” （工资 >= 20000 属于高新员工）
         // 12. 按部门统计员工姓名
@@ -103,11 +111,79 @@ public class StreamPractice6 {
         // 17. 技能 → 员工姓名（Java -> [张三, 李四, 王五, 赵六, 周九, 吴十, 冯十三]）
         // 18. 统计各部门工资分布。将工资分成：低薪：< 15000，中薪：15000 ~ 19999，高薪：>= 20000 要求形成二级 Map：Map<String, Map<String, Long>>
         // 19. 计算部门工资极差 定义工资极差：最高工资 - 最低工资
-        // 20. 每个部门中，掌握技能数量最多的员工。Map<String, Employee>
-        // 按部门分组
-        // 比较 skills.size()
-        // 技能数量相同时，工资高的人获胜
-        // 工资再相同时，年龄小的人获胜
+        // 20. 每个部门中，掌握技能数量最多的员工。Map<String, Employee>。按部门分组，比较 skills.size()，技能数量相同时，工资高的人获胜，工资再相同时，年龄小的人获胜
 
     }
+
+    // 1. 找出：研发部员工，工资大于 15000，按工资从高到低排序，最终只返回员工姓名
+    public static List<String> t1(List<Employee> employees) {
+
+        return employees.stream()
+                .filter(employee -> employee.salary() > 15000)
+                .sorted(Comparator.comparingDouble(Employee::salary).reversed())
+                .map(Employee::name)
+                .toList();
+    }
+
+    // 2. 从所有员工中找出工资最高的 3 名员工。工资降序，如果工资相同，年龄小的排前面
+    public static List<Employee> t2(List<Employee> employees) {
+
+        return employees.stream()
+                .sorted(Comparator.comparingDouble(Employee::salary).reversed()
+                        .thenComparing(Employee::age))
+                .limit(3)
+                .toList();
+    }
+
+    // 3. 将员工按照部门分组，并统计每个部门有多少人。
+    public static Map<String, Long> t3(List<Employee> employees) {
+
+        return employees.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::department,
+                        Collectors.counting()
+                ));
+    }
+
+    // 4. 按照部门统计平均工资。
+    public static Map<String, Double> t4(List<Employee> employees) {
+
+        return employees.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::department,
+                        Collectors.averagingDouble(Employee::salary)
+                ));
+    }
+
+    // 5. 按照部门分组，然后找出每个部门工资最高的员工
+    public static Map<String, Employee> t5(List<Employee> employees) {
+
+        return employees.stream()
+                .collect(Collectors.groupingBy(
+                        Employee::department,
+                        Collectors.collectingAndThen(
+                                Collectors.toList(),
+                                list -> list.stream()
+                                        .max(Comparator.comparingDouble(Employee::salary))
+                                        .orElse(null)
+                        )
+                ));
+    }
+
+    // 6. 定义三个年龄段：30岁以下、30~34岁、35岁及以上 按照年龄段对员工分组。
+    // 7. 统计全部员工的技能出现次数。
+    // 8. 找出出现次数最多的技能，在第 7 题基础上，进一步找出：被最多员工掌握的技能
+    // 9. 找出掌握 Java 但不会 Spring/SpringBoot 的员工
+    // 10. 部门工资总额排名
+    // 11. 判断部门是否存在“高薪员工” （工资 >= 20000 属于高新员工）
+    // 12. 按部门统计员工姓名
+    // 13. 找出工资重复的员工
+    // 14. 每个部门工资前两名
+    // 15. 找出平均工资最高的部门
+    // 16. 生成员工信息 Map （员工姓名 -> 工资）
+    // 17. 技能 → 员工姓名（Java -> [张三, 李四, 王五, 赵六, 周九, 吴十, 冯十三]）
+    // 18. 统计各部门工资分布。将工资分成：低薪：< 15000，中薪：15000 ~ 19999，高薪：>= 20000 要求形成二级 Map：Map<String, Map<String, Long>>
+    // 19. 计算部门工资极差 定义工资极差：最高工资 - 最低工资
+    // 20. 每个部门中，掌握技能数量最多的员工。Map<String, Employee>。按部门分组，比较 skills.size()，技能数量相同时，工资高的人获胜，工资再相同时，年龄小的人获胜
+
 }
